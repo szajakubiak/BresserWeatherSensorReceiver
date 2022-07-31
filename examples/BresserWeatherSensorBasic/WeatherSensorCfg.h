712c9ee965356b1b73964357ed370ca8361dedab
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2022-07-31 09:37:53 +0200

    Set pinout for TTGO LoRa

--- examples/BresserWeatherSensorBasic/WeatherSensorCfg.h
+++ examples/BresserWeatherSensorBasic/WeatherSensorCfg.h
@@ -1,6 +1,6 @@
 // Select appropriate sensor message format
-//#define BRESSER_5_IN_1
-#define BRESSER_6_IN_1
+#define BRESSER_5_IN_1
+//#define BRESSER_6_IN_1
 
 // Select type of receiver module
 // (RFM95W: USE_SX1276)
@@ -11,21 +11,22 @@
 
 // Arduino default SPI pins
 //
-// Board   SCK   MOSI  MISO
-// ESP8266 D5    D7    D6
-// ESP32   D18   D23   D19
+// Board     SCK   MOSI  MISO
+// ESP8266   D5    D7    D6
+// ESP32     D18   D23   D19
+// TTGO LoRa D5    D27   D19
 
 #if defined(ESP32)
-    #define PIN_RECEIVER_CS   27
+    #define PIN_RECEIVER_CS   18
     
     // CC1101: GDO0 / RFM95W/SX127x: G0
-    #define PIN_RECEIVER_IRQ  21 
+    #define PIN_RECEIVER_IRQ  26
     
     // CC1101: GDO2 / RFM95W/SX127x: G1
     #define PIN_RECEIVER_GPIO 33
     
     // RFM95W/SX127x - GPIOxx / CC1101 - RADIOLIB_NC
-    #define PIN_RECEIVER_RST  32
+    #define PIN_RECEIVER_RST  14
 
 #elif defined(ESP8266)
     #define PIN_RECEIVER_CS   15
